94944275920d311e2b98d94d4deef4298cd0d9a6
NickBerg <niccolo.berguglia@gmail.com>
2020-03-09 14:03:58 +0100

    I solved all the problems, and updated all the frameworks.
    'androidx.versionedparcelable:versionedparcelable'
    
    flutter clean
    flutter pub cache repair
    
    'androidx.core:core'
    
    	android/build.gradle: classpath 'com.android.tools.build:gradle:3.3.1', before there is '3.2.1'
    
    #In build.gradle in root of android directory there is minSdkVersion, change value with 21
    
    The Android Gradle plugin supports only Kotlin Gradle plugin version 1.3.0 and higher. The following dependencies do not satisfy the required version: root project 'android' -> org.jetbrains.kotlin:kotlin-gradle-plugin:1.2.71
    
    Go to Tools->Kotlin->Configure Kotlin Plugin Updates-> Check again and install
    restart and after go to Tools->Kotlin-> Configure Kotlin in Project-> modify all
    
    if the error subsist open build.gradle in android directory root and modify ext.kotlin_version with last kotlin version

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,7 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   _initChannel() async {
     Channel channel = await APIService.instance
-        .fetchChannel(channelId: 'UC6Dy0rQ6zDnQuHQ1EeErGUA');
+        .fetchChannel(channelId: 'UCiHohJG1XFFPF-De2QH7PZA');
     setState(() {
       _channel = channel;
     });
